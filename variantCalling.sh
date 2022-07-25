de075d14647199141cac0ea2d771fc518f4935ba
Shaima Ahmed <shaimatasneemahmed@gmail.com>
2022-07-25 15:16:08 +0530

    added .sh file

--- variantCalling.sh
+++ variantCalling.sh
@@ -58,10 +58,3 @@ java -Xmx6g -jar ./snpEff/snpEff.jar -v GRCh38.105 Merged_readfiles_variant.vcf 
 #variant annotation with clinvar
 gunzip clinvar.vcf.gz
 java -jar ./snpEff/SnpSift.jar annotate clinvar.vcf Merged_readfiles_variant_ann.vcf > Merged_readfiles_variant_dbanno.vcf
-
-
-
-
-
-
-
